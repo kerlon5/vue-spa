78b4493d584fc93e42391c1c9f3df53870791e69
Victor Naumov <naumov.victor@outlook.com>
2018-08-24 07:07:10 +0500

    add some style

--- src/theme/Layout.vue
+++ src/theme/Layout.vue
@@ -8,7 +8,23 @@
           </a>
         </div>
       </nav>
-      <section class="main-section section"></section>
+      <section class="main-section section">
+        <div class="container content">
+  <div class="columns">
+    <div class="column is-one-third" v-for="(post, title) in posts" v-bind:key="post.id">
+      <div class="card">
+        <div class="card-content">
+          <h3>{{ post.title }}</h3>
+          {{ post.content }}
+        </div>
+        <footer class="card-footer">
+          <a class="card-footer-item" :href="post.link" target="_blank">Read More</a>
+        </footer>
+      </div>
+    </div>
+  </div>
+</div>
+      </section>
       <footer class="footer">
         <div class="container">
           <div class="content has-text-centered">
@@ -21,5 +37,17 @@
     </div>
 </template>
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      posts: [
+        { id: 1, title: 'London', content: 'is the capital of the GB' },
+        { id: 2, title: 'Birmingham', content: 'yet another city' }
+      ]
+    }
+  }
+}
 </script>
+<style lang="scss">
+@import '~bulma';
+</style>
