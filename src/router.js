3ca06b3a3058559d83e5f63ba4daef45b7f08e3a
Victor Naumov <naumov.victor@outlook.com>
2018-08-24 19:13:47 +0500

    404

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import City from './theme/City.vue'
+import NotFound from './theme/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -8,9 +9,10 @@ const router = new VueRouter({
   mode: 'history',
   linkActiveClass: 'is-active',
   scrollBehavior: (to, from, savedPosition) => ({ y: 0 }),
-  routes: [{
-    path: '/', component: City
-  }]
+  routes: [
+    { path: '/', component: City },
+    { path: '*', component: NotFound }
+  ]
 })
 
 export default router
