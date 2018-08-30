2b60bb6a0df45db2656f85cfe72290ccd3caa58a
Victor Naumov <naumov.victor@outlook.com>
2018-08-30 22:53:07 +0500

    'dfdsf'

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import AppLayout from './theme/Layout.vue'
-import router from './router'
 
 const app = new Vue({
-  router,
   ...AppLayout
 })
 
-export { app, router }
+export { app }
